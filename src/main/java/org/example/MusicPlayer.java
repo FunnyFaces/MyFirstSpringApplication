ffb24511da5f7ecc44fe400cb8d08c17acf7b48d
Philip Nerubenko <f.nerubenko@gmail.com>
2022-07-18 14:28:01 +0300

    add set and arraylist

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,13 +1,43 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
     // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public MusicPlayer() {
+
+    }
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
